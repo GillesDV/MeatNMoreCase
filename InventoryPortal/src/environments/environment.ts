95b03495852f58d42c479517ff35da58d5dedfa3
Gilles <gilles.devylder@hotmail.com>
2026-05-05 20:58:39 +0200

    rotate and replace leaked key. Whoopsie 🙈

--- InventoryPortal/src/environments/environment.ts
+++ InventoryPortal/src/environments/environment.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: false,
   articleApiBaseUrl: 'https://localhost:7231/',
   firebaseAuth: {
-    apiKey: 'AIzaSyA1F6bNwFWDOy8KThJ3iOPqU6seubNT1EQ',
-    authDomain: 'fir-project-ec54c.firebaseapp.com',
-    projectId: 'fir-project-ec54c',
-    storageBucket: 'fir-project-ec54c.firebasestorage.app',
-    messagingSenderId: '625128373204',
-    appId: '1:625128373204:web:d4fce342fed99c70943441',
-    measurementId: 'G-VRZFQNTEDF'
+    apiKey: 'replace_me',
+    authDomain: 'replace_me',
+    projectId: 'replace_me',
+    storageBucket: 'replace_me',
+    messagingSenderId: 'replace_me',
+    appId: 'replace_me',
+    measurementId: 'replace_me'
   }
 };
